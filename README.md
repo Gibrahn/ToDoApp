cd7ea840a425c85ece611d6bceb9a45c134e5c5e
Gibrahn <gibrahn.duarte18@tectijuana.edu.mx>
2022-03-07 21:57:50 -0800

    TodoApp
    Added styles and js on how to add task

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ToDoApp
- ToDo app where the user can:   - Register a new Task using the prompt.  - Store the tasks in an array.  - Display the Task in the Todo task section, you have to use document.getElementById().  - Remove the task when you click on the button delete, and remove the element from the array.
+ ToDo app where the user can:   
+ - Register a new Task using the prompt.  
+ - Store the tasks in an array.  
+ - Display the Task in the Todo task section, you have to use document.getElementById().  
+ - Remove the task when you click on the button delete, and remove the element from the array.
---
+++ script.js
@@ -0,0 +1,25 @@
+let toDo = [];
+
+function addTask(){
+    // asks user for task
+    let task = prompt("Enter Task: ");
+    //adds task to array
+    toDo.push(task);
+    //display the tasks
+    displayTasks();
+}
+
+function displayTasks(){
+    //clear task list
+    document.getElementById("toDoList").innerHTML+="";
+    for(let x=0;x<toDo.length;x++){
+        //display the task
+        document.getElementById("toDoList").innerHTML+=`
+        <li id="${x}">${toDo[x]} <button onclick="deleteTask(${x});"> Delete </button> </li>
+        `;
+    }
+}
+function deleteTask(id){
+    document.getElementById(id).remove(); //remove element from HtmL
+    names.splice(id, 1);// remove the element from the array
+}
